44c62de00d8ac6a40bfc018a4e3660b5913ba12f
grandbora <bora.tunca@yahoo.com>
2013-06-06 22:47:57 +0200

    go on

--- Gesture/Gesture/TouchViewController.m
+++ Gesture/Gesture/TouchViewController.m
@@ -26,4 +26,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) touchesBegan:(NSSet *)touches
+            withEvent:(UIEvent *)event
+{
+    NSUInteger touchCount = [touches count];
+    NSUInteger tapCount = [[touches anyObject] tapCount];
+    _methodStatus.text = @"touchesBegan";
+    _touchStatus.text = [NSString stringWithFormat:
+                         @"%d touches", touchCount];
+    _tapStatus.text = [NSString stringWithFormat:
+                       @"%d taps", tapCount];
+}
+
+- (void) touchesMoved:(NSSet *)touches
+            withEvent:(UIEvent *)event
+{
+    NSUInteger touchCount = [touches count];
+    NSUInteger tapCount = [[touches anyObject] tapCount];
+    _methodStatus.text = @"touchesMoved";
+    _touchStatus.text = [NSString stringWithFormat:
+                         @"%d touches", touchCount];
+    _tapStatus.text = [NSString stringWithFormat:
+                       @"%d taps", tapCount];
+}
+
 @end
